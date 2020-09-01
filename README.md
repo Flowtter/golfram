ed435d6c93812577611223ed02a5520974f42e1a
Brice <naexys.naexys@gmail.com>
2020-09-01 15:56:58 +0200

    Fixed Readme

--- README.md
+++ README.md
@@ -11,9 +11,8 @@ If you feel like showing your appreciation for this project, then how about shou
 ## Get started
 
 * clone this repository
-* go build 
-* run `golfram.exe`
-* use the CLI
-
-## Command Line Input
-* TO-DO
+* To parse an expression, use semi-column. Space will be erased by Golfram, use them if it can help you. (2 + 3) == (2+3)
+* basic, b, and an expression to solve it. Example : " basic: 2 + 3 / 4 * 5 \" 
+* vector, v, to work on vectors. Example : " vector : (2,y)*z+(x,6)*2 \" \n
+* degree, d, to get the degree of an expression. Example : " degree : x->x^3+x^2+x+1 \" 
+* function, f, to get the result of the function with a parameter. Example : \" function : x->x+2 : 2 " 
--- main.go
+++ main.go
@@ -62,9 +62,9 @@ func request(expression string) string {
 	} else if len(parse) == 1 {
 		if parse[0] == "help" || parse[0] == "h" {
 			return `Welcome to Golfram \n
-			To parse an expression, use semi-column. Space will be deleted by Golfram, use them if it can help you.
+			To parse an expression, use semi-column. Space will be erased by Golfram, use them if it can help you. (2 + 3) == (2+3)
 			basic, b, and an expression to solve it. Example : \" basic: 2 + 3 / 4 * 5 \" \n
-			vector, v, to work on vector. Example : \" vectors : (2,y)*z+(x,6)*2 \" \n
+			vector, v, to work on vectors. Example : \" vector : (2,y)*z+(x,6)*2 \" \n
 			degree, d, to get the degree of an expression. Example : \" degree : x->x^3+x^2+x+1 \" \n
 			function, f, to get the result of the function with a parameter. Example : \" function : x->x+2 : 2 \" \n`
 		} else {
